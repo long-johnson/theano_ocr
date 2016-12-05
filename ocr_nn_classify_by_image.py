bc40fecc5e4214727c21895c44de5b872d796626
Vadim Uvarov <razoru93@gmail.com>
2016-12-05 15:36:16 +0700

    Fixed problem with printing grayscale images

--- ocr_nn_classify_by_image.py
+++ ocr_nn_classify_by_image.py
@@ -25,24 +25,19 @@ def label_to_char(label: int):
     return chr(65 + label - 10)
 
 out_dir = "out/"
-#incorrect_dir = out_dir + "incorrect_dir/"
-image_dir = "D:/python_projects/image_dir/"
-filename_params = "CNNflorian_complexity=1_n_epochs=65, batch_size=128, alpha=6.25000029685907e-05, lambd=0.01, mu=0.9900000095367432, acc_train=98.8, acc_val=88.3,dropout=0.05, seed=2.npz"
+class_results_dir = out_dir + "classify_res/"
+img_dir = "data/to_classify"
+filename_params = "CNNflorian-adam_complexity=1_n_epochs=173, batch_size=512, alpha=0.0008, lambd=0.01, mu=0.9, acc_train=98.9, acc_val=97.1,dropout=0.2, seed=1.npz"
 
 pic_size = 32
 
 img_original = []
 X = []
 
-for infile in glob.glob(os.path.join(image_dir, '*.*')):#+ ld[i]
-        img_original.append(Image.open(infile))
+for infile in glob.glob(os.path.join(img_dir, '*.*')):#+ ld[i]
+        img_original.append(Image.open(infile).convert('RGB'))
         im = Image.open(infile).convert('L')
         im = im.resize((pic_size, pic_size), Image.ANTIALIAS)
-        #im.save(tmp_path, "PNG")
-        
-        #im = io.imread(tmp_path)
-        #im = color.rgb2gray(im)
-        #misc.imsave(path.format(i, j), im)
         im = np.reshape(np.array(im.getdata(), dtype=np.float32), pic_size*pic_size)
         X.append(im)
 
@@ -52,20 +47,22 @@ batch_size = 128
 
 print("start")
 
-network, _, _, test_fn = nn_models.build_cnn_florian(pic_size, complexity=3)
+network, _, _, test_fn = nn_models.build_cnn_florian(pic_size, complexity=1)
 npz_file = np.load(out_dir + filename_params)
 params = [npz_file["arr_{}".format(i)] for i in range(len(npz_file.files))]
 lasagne.layers.set_all_param_values(network, params)
 
 print("model was built")
 
-if not os.path.exists(image_dir + "res/"):
-    os.makedirs(image_dir + "res/")
+if not os.path.exists(class_results_dir):
+    os.makedirs(class_results_dir)
 
+# form label strings
 class_labels = []
 for i in range(36):
     class_labels.append(label_to_char(i))
 
+# save pretty classification results
 step = 0
 for Xbatch in iterate_minibatches(X, batch_size):
     pred_labels, pred_vectors = test_fn(Xbatch)
@@ -77,7 +74,7 @@ for Xbatch in iterate_minibatches(X, batch_size):
         #fig, ax = plt.subplots()
         plt.barh(range(len(class_labels)), pred_vectors[i][::-1])
         plt.yticks(range(len(class_labels)), class_labels[::-1], rotation="vertical")
-        plt.savefig(image_dir + "res/" + "{}_actual={}.jpg".format(step * batch_size + i, label_to_char(pred_labels[i])))
+        plt.savefig(class_results_dir + "{}_actual={}.jpg".format(step * batch_size + i, label_to_char(pred_labels[i])))
         plt.clf()
         #img_original[step * batch_size + i].save(image_dir + "res/" + "{}_actual={}.jpg".format(step * batch_size + i, label_to_char(pred_labels[i])))
     print("step = {}".format(step))
